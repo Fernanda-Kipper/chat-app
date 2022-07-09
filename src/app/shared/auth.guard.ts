dce4125f03dc07f7301971a3e708b8366d4640e5
Fernanda Kipper <nanda.kipper@gmail.com>
2022-07-09 20:57:23 -0300

    fix: adjust auth guard to use auth service

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router){}
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ){}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
+      if(this.authService.isAuthenticated()) return true;
+
       this.router.navigate(['login'])
-    return false;
+      return false;
   }
 }
