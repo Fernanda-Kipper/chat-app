96e803571dc4061a3d604b33981c14522848d152
Fernanda Kipper <nanda.kipper@gmail.com>
2022-07-09 20:57:02 -0300

    feat: add loigin and chat

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { User } from 'src/app/shared/services/user';
 
 @Component({
   selector: 'app-chat',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  user!: User;
+
+  constructor(private authService: AuthService) {
+    const user = authService.getUser();
+    if(user) this.user = user;
+  }
 
   ngOnInit(): void {
   }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin(){
+    this.authService.logIn()
   }
 
 }
